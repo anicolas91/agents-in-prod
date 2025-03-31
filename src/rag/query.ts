c42252a3f4f8331c3866114f464d4e2849070546
anicolas91 <andreanicolas91@gmail.com>
2025-03-31 16:05:28 -0700

    refactor(query.ts): formatted it a bit better

--- src/rag/query.ts
+++ src/rag/query.ts
@@ -1,7 +1,10 @@
 import {Index as UpstashIndex} from '@upstash/vector'
 
 // initialize Upstash vector client
-const index = new UpstashIndex()
+const index = new UpstashIndex({
+  url: process.env.UPSTASH_VECTOR_REST_URL as string,
+  token: process.env.UPSTASH_VECTOR_REST_TOKEN as string,
+})
 
 // create a unique namespace, this time using the user id
 //index.namespace('dvuwy12g')
@@ -9,21 +12,23 @@ const index = new UpstashIndex()
 // create a query
 export const queryMovies = async ({
     query,
-    filters,
+    //filters,
     topK = 5, // top 5 most related relative to metric
 
 }:{ 
     query: string,
-    filters?: any,
+    //filters?: any,
     topK?: number,
 }) => {
     // loop through all filters and convert to stringse
 
-    // create query
-    return index.query({
+    // query the vector store
+    const results = await index.query({
         data: query,
         topK,
         includeData: true,
         includeMetadata: true,
     })
+
+    return results
 }
