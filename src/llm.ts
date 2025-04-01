28bcb891ab66259adbceac493e23b021ae97e699
anicolas91 <andreanicolas91@gmail.com>
2025-04-01 11:02:30 -0700

    feat(llm.ts): added an approval check fcn
    its a boolean that checks whether the user approved img generation or not

--- src/llm.ts
+++ src/llm.ts
@@ -1,7 +1,8 @@
 import type { AIMessage } from '../types'
 import { openai } from './ai'
-import { zodFunction } from 'openai/helpers/zod'
+import { zodFunction, zodResponseFormat } from 'openai/helpers/zod'
 import { systemPrompt as defaultSystemPrompt } from './systemPrompt'
+import { z } from 'zod'
 
 export const runLLM = async ({
   messages,
@@ -35,3 +36,22 @@ export const runLLM = async ({
 
   return response.choices[0].message
 }
+
+// create an approval check object
+ export const runApprovalCheck = async (userMessage: string) => {
+  const result = await openai.beta.chat.completions.parse({
+    model: 'gpt-4o-mini',
+    temperature: 0.1,
+    response_format: zodResponseFormat(z.object(
+      {approved: z.boolean().describe('did the user approve the action or not')}
+      ),
+    'approval'
+    ),
+    messages: [
+      {role:'system',content: 'Determine if the user approved the image generation. If you are not sure, then it is not approved.'},
+      {role: 'user', content: userMessage}]
+  })
+
+  return result.choices[0].message.parsed?.approved
+
+ }
